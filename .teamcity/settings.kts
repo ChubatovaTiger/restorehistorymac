6b283723a724396d3dc8ff575b4f45ab61eb2ecc
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2023-01-18 08:25:15 +0100

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -139,8 +139,8 @@ object Project12_Build1copy : BuildType({
 object Project13 : Project({
     name = "project13"
 
-    //buildType(Project13_B1)
-    buildType(Project13_B2x)
+    buildType(Project13_B1)
+    buildType(Project13_B2)
 
     subProject(Project13_Project131)
 })
@@ -166,8 +166,8 @@ object Project13_B1 : BuildType({
     }
 })
 
-object Project13_B2x : BuildType({
-    name = "newbt"
+object Project13_B2 : BuildType({
+    name = "b2"
 
     params {
         param("cat", "a")
