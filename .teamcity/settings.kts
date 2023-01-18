a6115ab3433c065fff8d9e813aeda48cc880bc52
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2023-01-18 13:01:13 +0100

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -4,25 +4,12 @@ import jetbrains.buildServer.configs.kotlin.buildSteps.script
 version = "2022.10"
 
 project {
-
-    //if (DslContext.getParameter(name = "buildTypePresence") != "false" )
-    //{
-        buildType(buid1)
-    //}
+buildType(buid1)
 }
 
 object build1 : BuildType({
-
-    name = "b11"
-
+name = "b11"
     params {
         param("par", DslContext.getParameter(name = "par"))
-    }
-
-    steps {
-        script {
-            id = "RUNNER_2"
-            scriptContent = "sleep 12"
-        }
     }
 })
