3b53b361c9d82a12675f448b38aae7a019a422d7
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2023-01-18 10:33:11 +0100

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -32,7 +32,7 @@ project {
 object B8 : BuildType({
     templates(AbsoluteId("Project1_B1_2"))
     name = "b11"
-    id("bsdlkjlkjjkjh")
+    id("bsdlkjlkjjkjhjhkjhkjh")
 
     params {
         param("aa", "aa")
